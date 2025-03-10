6e24b316d6ba402cc47894126f802bfd2b117066
DaikiTsuha <tsuha_daiki@ca-adv.co.jp>
2025-03-10 08:41:44 +0000

    その他の章作成。vercel テスト

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
